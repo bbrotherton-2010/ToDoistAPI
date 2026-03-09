be2b367d539eb7bcf9126f638fb7d2ce85a1c009
Brandy <blbst36@hotmail.com>
2026-03-09 14:12:06 -0400

    trying different ways of getting all the tasks using next cursor value instead of a total number

--- todoist.py
+++ todoist.py
@@ -1,8 +1,23 @@
 import todoistAPICalls as calls
+import json
+import pandas as pd
 
 
 api = calls.apiCalls()
 projectData = api.getAllProjects()
+taskData = api.getAllTasks()
 
 for project in projectData['results']:
-    print(project['name'])
+    print(f'Name: {project['name']}; Id: {project["id"]}')
+
+
+print(len(taskData))
+
+# if type(taskData['next_cursor']) == 'str':
+#
+
+
+# data_as_table = pd.DataFrame(taskData)
+#
+# print(data_as_table)
+
--- todoistAPICalls.py
+++ todoistAPICalls.py
@@ -10,15 +10,21 @@ class apiCalls:
         self.headers = {
             'Authorization': f'Bearer {self.apiKey}',
             'Content-Type': 'application/x-www-form-urlencoded',
+            'limit': 200
         }
         self.baseUrl = 'https://api.todoist.com/api/v1/'
 
-    def getCall(self, url):
+    def getCall(self, url, additionalQuery=''):
         headers = {
             'Authorization': f'Bearer {self.apiKey}',
             'Content-Type': 'application/x-www-form-urlencoded',
         }
-        todoistGet = requests.get(f'{self.baseUrl}{url}', headers=headers)
+        if len(additionalQuery) > 0:
+            query = f'{additionalQuery}&limit=200'
+        else:
+            query = '?limit=200'
+
+        todoistGet = requests.get(f'{self.baseUrl}{url}{query}', headers=headers)
         todoistGetJson = todoistGet.json()
         return todoistGetJson
 
@@ -27,3 +33,21 @@ class apiCalls:
         projects = self.getCall('projects')
 
         return projects
+
+    def getAllTasks(self):
+        tasks = self.getCall('tasks')
+        nextCursor = tasks['next_cursor']
+        count = 0
+
+        if len(nextCursor) > 0:
+            allData = tasks['results']
+            while not nextCursor:
+                taskHolder = self.getCall('tasks', f'?cursor={nextCursor}')
+                allData = allData | taskHolder['results']
+                nextCursor = taskHolder['next_cursor']
+                count = count+1
+            print(len(allData))
+            print (count)
+            return allData
+        else:
+            return tasks
