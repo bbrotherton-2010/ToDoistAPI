646996d4c88989bb258efd6f1e9b614e4433c967
Brandy <blbst36@hotmail.com>
2026-03-16 15:24:38 -0400

    Jotting down ideas for next steps

--- todoist.py
+++ todoist.py
@@ -11,4 +11,8 @@ for project in projectData['results']:
     print(f'Name: {project['name']}; Id: {project["id"]}')
 
 
-
+# TODO pull out due dates, parent, project, and content
+# TODO find all labels
+# TODO Update past due tasks based on labels
+# TODO Print out the updates tasks and new due date
+# TODO create tasks based on text document including due dates, parent, project, content, labels
